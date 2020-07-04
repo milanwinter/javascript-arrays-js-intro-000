3edbe687187d5e0e86e8fefcd3c06c9bce1146db
Milan Winter <milanwinter8@gmail.com>
2020-07-04 04:12:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,5 +11,5 @@ function addElementToEndOfArray(array,element) {
   return [...array, element]
 }
 function destructivelyAddElementToEndOfArray(array,element) {
-  return array.push(element)
+  array = array.push(element)
 }
