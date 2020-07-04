27071f964bbdc85eccda5ac9f4aea28963b7d6da
Milan Winter <milanwinter8@gmail.com>
2020-07-04 04:24:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,3 +22,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
   return array;
 }
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
