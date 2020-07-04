19fbcca5314582632dc5c123ce143c143a271642
Milan Winter <milanwinter8@gmail.com>
2020-07-04 03:25:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,7 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
 var icecreams = ['vanilla','chocolate',]
-var addElementToBeginningOfArray = ['strawberry',...icecreams]
+function addElementToBeginningOfArray(){
+  icecreams.unshift('strawberry')
+}
+
+
