0443133db733303be1195e461d717f0dc1939f60
Milan Winter <milanwinter8@gmail.com>
2020-07-04 03:41:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
-function addElementToBeginningOfArray(array,element) {
-['element'...array]
+function addElementToBeginningOfArray(Array,element) {
+['element'...Array]
 }
